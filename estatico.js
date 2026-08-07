de3f61b421eee5612cb81958729f2db3e29ce6bd
FranklinResende <frankfabricatto@gmail.com>
2026-08-06 22:28:46 -0300

    Update viewer.js version in estatico.js

--- estatico.js
+++ estatico.js
@@ -236,6 +236,6 @@
   document.body.appendChild(dados);
 
   const js = document.createElement('script');
-  js.src = 'viewer.js?v=1786061937';
+  js.src = 'viewer.js?v=1786070000';
   document.body.appendChild(js);
 })();
